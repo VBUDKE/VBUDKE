e3c873f2eb91bf305ddfcd423d4d9ef56b1712fc
VBUDKE <mvbudaev@miem.hse.ru>
2022-06-27 08:33:10 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,9 @@
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../src/Source.cpp"
+ auto TEST_CASE("cchecking check conflict with tail")
+ {
+	  size_snake = 0;
+	 
+	CHECK(check_conflict_with_tail_() == false );
+ }
